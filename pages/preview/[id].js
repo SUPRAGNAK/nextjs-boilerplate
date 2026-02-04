06178b0724a33860e0845f0c6720daf13301d818
SUPRAGNAK <43203454+SUPRAGNAK@users.noreply.github.com>
2026-02-05 01:44:58 +0530

    Update [id].js

--- pages/preview/[id].js
+++ pages/preview/[id].js
@@ -12,13 +12,16 @@ export async function getServerSideProps({ query }) {
 
 export default function PreviewPage({ html }) {
   return (
-    <iframe
-      srcDoc={html}
-      style={{
-        width: "100%",
-        height: "100vh",
-        border: "none"
-      }}
-    />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <iframe
+        srcDoc={html}
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none"
+        }}
+        sandbox="allow-same-origin allow-forms"
+      />
+    </div>
   );
 }
